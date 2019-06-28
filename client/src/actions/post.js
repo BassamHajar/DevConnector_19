de4abddbe17e1c8f5904ff774e6895a9b1a4bd19
Bassam <b2dndr1@gmail.com>
2019-06-28 23:53:58 +0200

    Like & Unlike Functionality

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -3,7 +3,7 @@ import { setAlert } from './alert';
 import {
   GET_POSTS,
   POST_ERROR,
-  // UPDATE_LIKES,
+  UPDATE_LIKES,
   // DELETE_POST,
   // ADD_POST,
   // GET_POST,
@@ -29,38 +29,38 @@ export const getPosts = () => async dispatch => {
 };
 
 // Add like
-// export const addLike = id => async dispatch => {
-//   try {
-//     const res = await axios.put(`/api/posts/like/${id}`);
+export const addLike = id => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
 
-//     dispatch({
-//       type: UPDATE_LIKES,
-//       payload: { id, likes: res.data },
-//     });
-//   } catch (err) {
-//     dispatch({
-//       type: POST_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status },
-//     });
-//   }
-// };
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 
 // Remove like
-// export const removeLike = id => async dispatch => {
-//   try {
-//     const res = await axios.put(`/api/posts/unlike/${id}`);
+export const removeLike = id => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${id}`);
 
-//     dispatch({
-//       type: UPDATE_LIKES,
-//       payload: { id, likes: res.data },
-//     });
-//   } catch (err) {
-//     dispatch({
-//       type: POST_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status },
-//     });
-//   }
-// };
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 
 // Delete post
 // export const deletePost = id => async dispatch => {
