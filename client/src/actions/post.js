7c61e62cb526a72354e7dcf0ec85798bb4388d05
Bassam <b2dndr1@gmail.com>
2019-06-28 23:57:11 +0200

    Delete Post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -4,7 +4,7 @@ import {
   GET_POSTS,
   POST_ERROR,
   UPDATE_LIKES,
-  // DELETE_POST,
+  DELETE_POST,
   // ADD_POST,
   // GET_POST,
   // ADD_COMMENT,
@@ -63,23 +63,23 @@ export const removeLike = id => async dispatch => {
 };
 
 // Delete post
-// export const deletePost = id => async dispatch => {
-//   try {
-//     await axios.delete(`/api/posts/${id}`);
+export const deletePost = id => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/${id}`);
 
-//     dispatch({
-//       type: DELETE_POST,
-//       payload: id,
-//     });
+    dispatch({
+      type: DELETE_POST,
+      payload: id,
+    });
 
-//     dispatch(setAlert('Post Removed', 'success'));
-//   } catch (err) {
-//     dispatch({
-//       type: POST_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status },
-//     });
-//   }
-// };
+    dispatch(setAlert('Post Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 
 // Add post
 // export const addPost = formData => async dispatch => {
