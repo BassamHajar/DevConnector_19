44802f56297136eeae3eaf1f9372f2a557d13315
Bassam <b2dndr1@gmail.com>
2019-06-29 00:02:56 +0200

    Adding Post

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,7 +5,7 @@ import {
   POST_ERROR,
   UPDATE_LIKES,
   DELETE_POST,
-  // ADD_POST,
+  ADD_POST,
   // GET_POST,
   // ADD_COMMENT,
   // REMOVE_COMMENT,
@@ -82,29 +82,29 @@ export const deletePost = id => async dispatch => {
 };
 
 // Add post
-// export const addPost = formData => async dispatch => {
-//   const config = {
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   };
+export const addPost = formData => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
-//   try {
-//     const res = await axios.post('/api/posts', formData, config);
+  try {
+    const res = await axios.post('/api/posts', formData, config);
 
-//     dispatch({
-//       type: ADD_POST,
-//       payload: res.data,
-//     });
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
 
-//     dispatch(setAlert('Post Created', 'success'));
-//   } catch (err) {
-//     dispatch({
-//       type: POST_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status },
-//     });
-//   }
-// };
+    dispatch(setAlert('Post Created', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 
 // Get post
 // export const getPost = id => async dispatch => {
