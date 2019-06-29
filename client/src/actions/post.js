d890f1a2a92a116d57270f41ca1b8df580b8ba4b
Bassam <b2dndr1@gmail.com>
2019-06-29 13:09:28 +0200

    Adding Comments

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -7,8 +7,8 @@ import {
   DELETE_POST,
   ADD_POST,
   GET_POST,
-  // ADD_COMMENT,
-  // REMOVE_COMMENT,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from './types';
 
 // Get posts
@@ -124,45 +124,45 @@ export const getPost = id => async dispatch => {
 };
 
 // Add comment
-// export const addComment = (postId, formData) => async dispatch => {
-//   const config = {
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   };
+export const addComment = (postId, formData) => async dispatch => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
 
-//   try {
-//     const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
+  try {
+    const res = await axios.post(`/api/posts/comment/${postId}`, formData, config);
 
-//     dispatch({
-//       type: ADD_COMMENT,
-//       payload: res.data,
-//     });
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data,
+    });
 
-//     dispatch(setAlert('Comment Added', 'success'));
-//   } catch (err) {
-//     dispatch({
-//       type: POST_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status },
-//     });
-//   }
-// };
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 
 // Delete comment
-// export const deleteComment = (postId, commentId) => async dispatch => {
-//   try {
-//     await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
+export const deleteComment = (postId, commentId) => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
-//     dispatch({
-//       type: REMOVE_COMMENT,
-//       payload: commentId,
-//     });
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId,
+    });
 
-//     dispatch(setAlert('Comment Removed', 'success'));
-//   } catch (err) {
-//     dispatch({
-//       type: POST_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status },
-//     });
-//   }
-// };
+    dispatch(setAlert('Comment Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
